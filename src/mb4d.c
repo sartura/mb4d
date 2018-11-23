a67502bea6ce8bd566c2c76521cf5e8ad1e93281
Domagoj Pintaric <domagoj.pintaric@sartura.hr>
2018-11-23 13:15:51 +0100

    add more comments and rename some variables
    Signed-off-by: Domagoj Pintaric <domagoj.pintaric@sartura.hr>

--- src/mb4d.c
+++ src/mb4d.c
@@ -100,6 +100,7 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
+	// check that iptv ifname interface exists and get it's ifindex
 	error = iptv_ifindex = if_nametoindex(iptv_ifname);
 	if (error == 0) {
 		_error("if_nametoindex error: %s", strerror(errno));
@@ -107,6 +108,7 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
+	// check that wan ifname interface exists and get it's ifindex
 	error = wan_ifindex = if_nametoindex(wan_ifname);
 	if (error == 0) {
 		_error("if_nametoindex error: %s", strerror(errno));
@@ -114,6 +116,7 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
+	// parse mld request source address
 	mld_request_source.sin6_family = AF_INET6;
 	error = inet_pton(AF_INET6, mld_request_source_address, &(mld_request_source.sin6_addr));
 	if (error != 1) {
@@ -122,16 +125,19 @@ int main(int argc, char **argv)
 	}
 	// mld_request_source.sin6_port = 0;
 
+	// initialize socket used for intercepting IGMP packets
 	error = iptv_igmp_receive_socket_init();
 	if (error < 0) {
 		exit_cb(SIGABRT);
 	}
 
+	// initialize socket used for sending decapsulated multicast packets
 	error = iptv_multicast_send_socket_init();
 	if (error < 0) {
 		exit_cb(SIGABRT);
 	}
 
+	// initialize socket used for receiving encapsulated multicast packets
 	error = wan_multicast_socket_init();
 	if (error < 0) {
 		exit_cb(SIGABRT);
@@ -167,6 +173,10 @@ int main(int argc, char **argv)
 
 static void exit_cb(int signal)
 {
+	// NOTE:
+	// - closing sockets is not strictly necessary since they will be closed by the OS when the program terminates but it's a good practice
+	// - also note that all multicast memberships (associated with a socket) are dropped when a socket is closed
+
 	if (iptv_igmp_receive_socket > 0) {
 		close(iptv_igmp_receive_socket);
 	}
@@ -188,8 +198,8 @@ static void exit_cb(int signal)
 static int iptv_igmp_receive_socket_init(void)
 {
 	// NOTE:
-	// - we use AF_PACKET + ETH_P_IP to intercept all IGMP packets
-	// - another option to get all IGMP packets would be to open a control socket for kernel multicast routing table using MRT_INIT socket options
+	// - AF_PACKET + ETH_P_IP s used to intercept all IGMP packets
+	// - another option to get all IGMP packets would be to open a control socket for kernel multicast routing table using MRT_INIT socket option
 	// - but since there can be only one socket on the system on which the call MRT_INIT succeeds that soultion is not feasible
 
 	int error = 0;
@@ -204,7 +214,8 @@ static int iptv_igmp_receive_socket_init(void)
 	bind_address.sll_family = AF_PACKET;
 	bind_address.sll_protocol = htons(ETH_P_IP);
 	bind_address.sll_ifindex = (int) iptv_ifindex;
-	error = bind(iptv_igmp_receive_socket, (struct sockaddr *) &bind_address, sizeof(bind_address));
+
+	error = bind(iptv_igmp_receive_socket, (struct sockaddr *) &bind_address, sizeof bind_address);
 	if (error < 0) {
 		_error("bind error: %s", strerror(errno));
 		return -1;
@@ -216,8 +227,8 @@ static int iptv_igmp_receive_socket_init(void)
 static int iptv_multicast_send_socket_init(void)
 {
 	// NOTE:
-	// - we use this socket for sending out received multicast packets from wan
-	// - since the received ipv6 packet has an encapsulated ipv4 multicast packet we have two options:
+	// - this socket s used for sending out decapsulated multicast packets received on wan
+	// - since the received ipv6 packet has an encapsulated ipv4 multicast packet there are two options:
 	// a) create a raw socket and forward bytes from the start of ipv4 header
 	// b) create a UDP multicast socket and send a multicast packet using udp payload
 
@@ -264,26 +275,28 @@ static void iptv_igmp_receive(void)
 {
 	static unsigned char datagram[1024 * 8] = {0};
 
-	ssize_t datagram_size = recv(iptv_igmp_receive_socket, datagram, sizeof datagram, 0);
-
-	if (datagram_size < 0) {
+	ssize_t bytes_received = recv(iptv_igmp_receive_socket, datagram, sizeof datagram, 0);
+	if (bytes_received < 0) {
 		_error("recv error: %s", strerror(errno));
 		return;
 	}
 
+	// check enough bytes are received for ipv4 header parsing
 	const unsigned char *ipv4_header_start = datagram;
-	if (ipv4_header_start + sizeof(struct iphdr) > (datagram + datagram_size)) {
+	if (ipv4_header_start + sizeof(struct iphdr) > (datagram + bytes_received)) {
 		_error("ipv4 header size overflows datagram size");
 		return;
 	}
 
+	// check ipv4 protocol is IGMP
 	const unsigned char *ip_protocol_offset = ipv4_header_start + offsetof(struct iphdr, protocol);
 	if (*ip_protocol_offset != IPPROTO_IGMP) {
 		return;
 	}
 
+	// check enough bytes are received for igmp header inspection
 	const unsigned char *igmp_header_start = datagram + ((*ipv4_header_start & 0x0F) * 4);
-	if (igmp_header_start + sizeof(struct igmphdr) > (datagram + datagram_size)) {
+	if (igmp_header_start + sizeof(struct igmphdr) > (datagram + bytes_received)) {
 		_error("ipv4 header size overflows datagram size");
 		return;
 	}
@@ -291,7 +304,7 @@ static void iptv_igmp_receive(void)
 	const unsigned char *igmp_type_offset = igmp_header_start + offsetof(struct igmphdr, type);
 	const unsigned char *igmp_group_address_offset = igmp_header_start + offsetof(struct igmphdr, group);
 
-	// join or lave action
+	// translate IGMPv2 message into MLDv2
 	int mld_request_action;
 	switch (*igmp_type_offset) {
 		case IGMPV2_HOST_MEMBERSHIP_REPORT:
@@ -309,35 +322,38 @@ static void iptv_igmp_receive(void)
 
 	int error = 0;
 
-	// craft ipv6 group address based on ipv4 grouup address from IGMP request
-	char group_address_ipv4_str[INET_ADDRSTRLEN] = {0};
-	if (!inet_ntop(AF_INET, igmp_group_address_offset, group_address_ipv4_str, sizeof group_address_ipv4_str)) {
+	// craft ipv6 group address based on ipv4 grouup address from IGMP message
+	// ref: rfc8114
+	char ipv4_group_address_str[INET_ADDRSTRLEN] = {0};
+	if (!inet_ntop(AF_INET, igmp_group_address_offset, ipv4_group_address_str, sizeof ipv4_group_address_str)) {
 		_error("inet_ntop error: %s", strerror(errno));
 		return;
 	}
-	_trace("ipv4 group address: %s", group_address_ipv4_str);
+	_trace("ipv4 group address: %s", ipv4_group_address_str);
 
-	char group_address_ipv6_str[INET6_ADDRSTRLEN] = {0};
-	strcat(group_address_ipv6_str, mld_request_group_address_prefix);
-	strcat(group_address_ipv6_str, group_address_ipv4_str);
-	_trace("ipv6 group address: %s", group_address_ipv6_str);
+	char ipv6_group_address_str[INET6_ADDRSTRLEN] = {0};
+	strcat(ipv6_group_address_str, mld_request_group_address_prefix);
+	strcat(ipv6_group_address_str, ipv4_group_address_str);
+	_trace("ipv6 group address: %s", ipv6_group_address_str);
 
-	// craft and send out an SSM MLD request on wan_mld_send_socket
+	// craft and send out an SSM MLDv2 meesage using struct group_source_req
+	// ref: rfc3678
 	struct group_source_req mld_request = {0};
+
 	mld_request.gsr_interface = wan_ifindex;
-	memcpy(&mld_request.gsr_source, &mld_request_source, sizeof(mld_request_source));
+
+	memcpy(&mld_request.gsr_source, &mld_request_source, sizeof mld_request_source);
 
 	struct sockaddr_in6 *group_address = (struct sockaddr_in6 *) &mld_request.gsr_group;
 	group_address->sin6_family = AF_INET6;
-	error = inet_pton(AF_INET6, group_address_ipv6_str, &(group_address->sin6_addr));
+	error = inet_pton(AF_INET6, ipv6_group_address_str, &(group_address->sin6_addr));
 	if (error != 1) {
 		_error("inet_pton error: %s", strerror(errno));
 		return;
 	}
 	// group_address->sin6_port = 0;
 
-	// send SSM MLD request on wan
-	error = setsockopt(wan_multicast_socket, IPPROTO_IPV6, mld_request_action, &mld_request, sizeof(mld_request));
+	error = setsockopt(wan_multicast_socket, IPPROTO_IPV6, mld_request_action, &mld_request, sizeof mld_request);
 	if (error < 0) {
 		_error("setsockopt error: %s", strerror(errno));
 		return;
@@ -349,50 +365,50 @@ static void wan_multicast_receive(void)
 	static unsigned char datagram[1024 * 8] = {0};
 
 	struct sockaddr_in6 ipv6_source_address = {0};
-	ssize_t datagram_size = recvfrom(wan_multicast_socket,
-									 datagram,
-									 sizeof datagram,
-									 0,
-									 (struct sockaddr *) &ipv6_source_address,
-									 &(socklen_t){sizeof ipv6_source_address});
-
-	if (datagram_size < 0) {
+	ssize_t bytes_received = recvfrom(wan_multicast_socket,
+									  datagram,
+									  sizeof datagram,
+									  0,
+									  (struct sockaddr *) &ipv6_source_address,
+									  &(socklen_t){sizeof ipv6_source_address});
+	if (bytes_received < 0) {
 		_error("recvfrom error: %s", strerror(errno));
 		return;
 	}
 
+	// check received source address
 	if (!IN6_ARE_ADDR_EQUAL(&mld_request_source.sin6_addr, &ipv6_source_address.sin6_addr)) {
 		return;
 	}
 
+	// check enough bytes are received for ipv4 header parsing
 	const unsigned char *ipv4_header_start = datagram;
-	if (ipv4_header_start + sizeof(struct iphdr) > datagram + datagram_size) {
+	if (ipv4_header_start + sizeof(struct iphdr) > datagram + bytes_received) {
 		_error("ipv4 header size overflows datagram size");
 		return;
 	}
 
 	uint16_t ipv4_total_length;
 	const unsigned char *ipv4_total_length_offset = ipv4_header_start + offsetof(struct iphdr, tot_len);
-	memcpy(&ipv4_total_length, ipv4_total_length_offset, sizeof(ipv4_total_length));
+	memcpy(&ipv4_total_length, ipv4_total_length_offset, sizeof ipv4_total_length);
 
 	uint32_t ipv4_destination_address;
 	const uint8_t *ipv4_destination_address_offset = ipv4_header_start + offsetof(struct iphdr, daddr);
-	memcpy(&ipv4_destination_address, ipv4_destination_address_offset, sizeof(ipv4_destination_address));
+	memcpy(&ipv4_destination_address, ipv4_destination_address_offset, sizeof ipv4_destination_address);
 
 	// NOTE:
-	// - based on sendto_address kernel will add L2 address
+	// - based on ipv4 sendto_address kernel will add L2 address
 	struct sockaddr_in sendto_address = {0};
 	sendto_address.sin_family = AF_INET;
 	sendto_address.sin_addr.s_addr = ipv4_destination_address;
 
-	ssize_t sent = sendto(iptv_multicast_send_socket,
-						  ipv4_header_start,
-						  ntohs(ipv4_total_length),
-						  0,
-						  (struct sockaddr *) &sendto_address,
-						  sizeof(sendto_address));
-
-	if (sent < 0) {
+	ssize_t bytes_sent = sendto(iptv_multicast_send_socket,
+								ipv4_header_start,
+								ntohs(ipv4_total_length),
+								0,
+								(struct sockaddr *) &sendto_address,
+								sizeof sendto_address);
+	if (bytes_sent < 0) {
 		_error("sendto error: %s", strerror(errno));
 		return;
 	}
